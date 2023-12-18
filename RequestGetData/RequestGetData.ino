39ca2eb1e8e607dac7c9badfda6f5261ffd1409d
Gabriel Ivan <gabriel.ivan5795@gmail.com>
2023-12-18 22:26:21 +0200

    arduino ide test requests

--- RequestGetData/RequestGetData.ino
+++ RequestGetData/RequestGetData.ino
@@ -1,46 +1,115 @@
+/*
+  Complete details at https://RandomNerdTutorials.com/esp32-useful-wi-fi-functions-arduino/
+*/
+
 #include <WiFi.h>
 #include <HTTPClient.h>
 
-const char* ssid = "YOUR_WIFI_SSID"; 
-const char* password = "YOUR_WIFI_PASSWORD"; 
+// Replace with your network credentials (STATION)
+const char* ssid = "DIGI-w5HE-2.4G";
+const char* password = "Nwzw94W4";
 
-const char* serverUrl = "http://example.com"; 
+String serverName = "https://api.nasa.gov/planetary/apod?api_key=JclJYCz2wFMdWaOz3F6jPJLkbNpCpmZudYyyHrZv";
+String sensiboEndpoint = "https://home.sensibo.com/api/v2/pods/guHKtnvn/historicalMeasurements?apiKey=VCuPCe4p2xajYA4o9AaiL2SswbtgMa";
+//String sensiboEndpoint = "https://home.sensibo.com/api/v2/pods/guHKtnvn/acStates?apiKey=VCuPCe4p2xajYA4o9AaiL2SswbtgMa";
+unsigned long lastTime = 0;
+//one day
+//unsigned long timerDelay = 86400000;
+unsigned long timerDelay = 10000;
 
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+void createAPODRequest() {
+  if ((millis() - lastTime) > timerDelay) {
+    //Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){
+      HTTPClient http;
+      
+      // Your Domain name with URL path or IP address with path
+      http.begin(serverName.c_str());
+      
+      // If you need Node-RED/server authentication, insert user and password below
+      //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
+      
+      // Send HTTP GET request
+      int httpResponseCode = http.GET();
+      
+      if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        String payload = http.getString();
+        Serial.println(http.header("X-Ratelimit-Remaining"));
+        Serial.println(payload);
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+      // Free resources
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
+}
+
+void createSensiboRequest() {
+  if(WiFi.status()== WL_CONNECTED){
+      HTTPClient http;
+      
+      // Your Domain name with URL path or IP address with path
+      http.begin(sensiboEndpoint.c_str());
+      
+      // If you need Node-RED/server authentication, insert user and password below
+      //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
+      
+      // Send HTTP GET request
+      int httpResponseCode = http.GET();
+      
+      if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        //String payload = http.getString();
+        WiFiClient * stream = http.getStreamPtr();
+        String payload = "";
+        while (stream->available()) {
+          char c = stream->read();
+          payload += c;
+        }
+        Serial.println(payload);
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+      // Free resources
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+}
 
 void setup() {
-
+  Serial.begin(115200);
+  initWiFi();
+  Serial.print("RRSI: ");
+  Serial.println(WiFi.RSSI());
+  //createSensiboRequest();
 }
 
 void loop() {
-  Serial.begin(115200); 
-WiFi.begin(ssid, password); 
-while (WiFi.status() != WL_CONNECTED) {
-  delay(500);
-  Serial.print(".");
-}
-Serial.println("");
-Serial.print("Connected to WiFi network with IP Address: ");
-Serial.println(WiFi.localIP()); 
-
-
-HTTPClient http;
-http.begin(serverUrl);
-
-// Example of GET request
-int httpResponseCode = http.GET(); 
-
-// Example of POST request
-http.addHeader("Content-Type", "application/json"); 
-String requestBody = "{\"name\":\"ESP32\",\"value\":100}";
-httpResponseCode = http.POST(requestBody); 
-
-String payload = http.getString(); 
-
-Serial.print("HTTP Response code: ");
-Serial.println(httpResponseCode);
-Serial.println(payload); 
-
-http.end(); 
-
-delay(10000); 
+  createAPODRequest();
+  //createSensiboRequest();
 }
